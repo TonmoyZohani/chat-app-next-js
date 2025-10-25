f77ed4286a6151ef3eff7ca7f70269a0d8f8a729
TonmoyZohani <tonmoyzohani@gmail.com>
2025-10-25 18:24:59 +0600

    chatform created

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import ChatForm from "@/components/ChatForm";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex mt-24 justify-center w-full">
+      <div className="w-full max-w-3xl mx-auto">
+        <h1 className="mb-4 text-2xl font-bold">Room 1</h1>
+        <div>TODO:: Add Chat Room</div>
+        <ChatForm />
+      </div>
+    </div>
+  );
 }
---
+++ components/ChatForm.tsx
@@ -0,0 +1,27 @@
+"use client"
+import React, { useState } from "react";
+
+const ChatForm = () => {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (message.trim() !== "") {
+      setMessage("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex gap-2 mt-4">
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        className="flex-1 px-4 border-2 py-2 rounded-lg focus:outline-none"
+        placeholder="Type your message here..."
+      />
+    </form>
+  );
+};
+
+export default ChatForm;
